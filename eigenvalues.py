9d1075d324f6193d1369273f554ba5c601b183d2
Diego Sanchez <sanchez97sr@gmail.com>
2026-04-24 23:30:04 -0600

    version final

--- eigenvalues.py
+++ eigenvalues.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
-import numpy as np
 from qr import qr
 from gram_schmidt import matmul
 
-def eigenvals(A:list[list[float]], n:int = 100, tolerance = 1e-10)->list[float]:
+def eigenvals_simple(A:list[list[float]], n:int = 100)->list[float]:
     """
     Realiza n iteraciones del algoritmo QR para calcular 
     los eigenvalores de A
@@ -19,7 +18,44 @@ def eigenvals(A:list[list[float]], n:int = 100, tolerance = 1e-10)->list[float]:
         Q,R = qr(Ak)
         #producto invertido de R * Q
         Ak =  matmul(R, Q)
-        eigenvalores = np.diag(Ak).tolist()
+
+    eigenvalores = [Ak[i][i] for i in range(len(A))]
+
+    return eigenvalores
+
+def eigenvals(A: list[list[float]], n: int = 100, tolerance: float = 1e-10):
+    """
+    Realiza hasta n iteraciones del algoritmo QR para calcular 
+    los eigenvalores de A, ocupando una tolerancia
+    """
+    Ak = A 
+    dim = len(Ak)
+    
+    for iteracion in range(n):
+        # Obtenemos Q y R
+        Q, R = qr(Ak)
+        Ak = matmul(R, Q)
+        
+        # Verificamos la convergencia 
+        ha_convergido = True
+        for i in range(dim):
+            # j empieza en i + 1 para revisar solo arriba de la diagonal
+            for j in range(i + 1, dim): 
+                # Si algún valor absoluto es mayor o igual a la tolerancia, no ha convergido
+                if abs(Ak[i][j]) >= tolerance:
+                    ha_convergido = False
+                    break 
+            
+            if not ha_convergido:
+                break 
+                
+        # Si la bandera sigue siendo True, todos los valores fuera de la diagonal cumplen la condición
+        if ha_convergido:
+            print(f"Convergencia alcanzada en la iteración: {iteracion + 1}")
+            break
+
+    eigenvalores = [Ak[i][i] for i in range(dim)]
+    
     return eigenvalores
 
 #Prueba
@@ -30,8 +66,8 @@ if __name__=="__main__":
         [-2.0, 8.0]
     ]
     #con 10 iteraciones 
-    valores_propios = eigenvals(A_prueba, n=10)
+    valores_propios = eigenvals_simple(A_prueba, n=10)
     print(valores_propios)
-
-    
+    valores_propios2 = eigenvals(A_prueba)
+    print(valores_propios2)
 
